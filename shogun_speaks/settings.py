97cd17b98962cedb10ea3ad8ba4e9aeeeef3baf5
Pluffy2217 <prathamkl17@gmail.com>
2022-06-15 20:39:03 +0530

    aws-1

--- shogun_speaks/settings.py
+++ shogun_speaks/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r(jehi2x*oz$&0q+k)y9vk%#btb!_)yt$alt_#%dtm0k2884w8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
